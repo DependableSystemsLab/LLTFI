ca674957c14a8088ff3066ed1e77ea2a9ebbf57a
AnushreeBannadabhavi <anushree.bannadabhavi@gmail.com>
2022-12-22 11:59:12 -0800

    Fixed hard coded paths

--- sample_programs/ml_sample_programs/vision_models/googlenet-12/compile.sh
+++ sample_programs/ml_sample_programs/vision_models/googlenet-12/compile.sh
@@ -17,7 +17,7 @@ clang++ -DONNX_ML=1 input.c -o main.ll -O0 -S -emit-llvm -lonnx_proto -lprotobuf
 llvm-link -o model.ll -S main.ll model.mlir.ll
 
 printf "\n[Compile Script]: Generate model.exe \n"
-/home/llvm-project/build/bin/llc -filetype=obj -o model.o model.ll -O0 --relocation-model=pic
-clang++ -o model.exe model.o -L/home/LLTFI/LLTFI/build/bin/../runtime_lib -lllfi-rt -lpthread -L /Debug/lib -Wl,-rpath /home/LLTFI/LLTFI/build/bin/../runtime_lib -I$ONNX_MLIR_SRC/include -O0 -lonnx_proto -lprotobuf -lcruntime -ljson-c
+$LLVM_DST_ROOT/bin/llc -filetype=obj -o model.o model.ll -O0 --relocation-model=pic
+clang++ -o model.exe model.o -L$LLFI_BUILD_ROOT/bin/../runtime_lib -lllfi-rt -lpthread -L /Debug/lib -Wl,-rpath $LLFI_BUILD_ROOT/bin/../runtime_lib -I$ONNX_MLIR_SRC/include -O0 -lonnx_proto -lprotobuf -lcruntime -ljson-c
 
 printf "\n[Compile Script]: Compilation complete\n"

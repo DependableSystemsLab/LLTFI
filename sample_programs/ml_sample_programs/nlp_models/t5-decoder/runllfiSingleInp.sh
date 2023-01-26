bc3cccd666e44495230d2181c526d8c4907f2cae
Anushree Bannadabhavi <anushree.bannadabhavi@gmail.com>
2023-01-25 20:19:22 -0800

    t5-decoder: add tokenizer scripts
    * t5-decoder changes
    
    * t5-decoder changes
    
    * t5-decoder changes
    
    * Fixed hard-coded paths
    
    * Minor fixes
    
    Co-authored-by: uditagarwal97 <uditagarwal1997@gmail.com>

---
+++ sample_programs/ml_sample_programs/nlp_models/t5-decoder/runllfiSingleInp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf llfi*
+inputSample=$1
+
+cp -r "inputs/inp$inputSample/input_0.pb" .
+cp -r "inputs/inp$inputSample/input_1.pb" .
+
+$LLFI_BUILD_ROOT/bin/instrument --readable -L $ONNX_MLIR_BUILD/Debug/lib -lcruntime -ljson-c -lonnx_proto -lprotobuf model.ll
+$LLFI_BUILD_ROOT/bin/profile ./llfi/model-profiling.exe input_0.pb input_1.pb 0
+$LLFI_BUILD_ROOT/bin/injectfault ./llfi/model-faultinjection.exe input_0.pb input_1.pb 0

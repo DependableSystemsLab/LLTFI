be9a62f66e60a7553b2d5628a5571d670e727d02
Anushree Bannadabhavi <anushree.bannadabhavi@gmail.com>
2023-01-26 18:27:38 -0800

    t5-encoder: add tokenizer scripts
    Co-authored-by: uditagarwal97 <uditagarwal1997@gmail.com>

--- sample_programs/ml_sample_programs/nlp_models/t5-encoder/compile.sh
+++ sample_programs/ml_sample_programs/nlp_models/t5-encoder/compile.sh
@@ -8,16 +8,24 @@ else
     wget https://github.com/onnx/models/raw/main/text/machine_comprehension/t5/model/t5-encoder-12.onnx
 fi
 
+DEC_FILE=t5-decoder-with-lm-head-12.onnx
+if [ -f "$DEC_FILE" ]; then
+    echo "$DEC_FILE exists."
+else
+    echo "$DEC_FILE does not exist."
+    wget https://github.com/onnx/models/raw/main/text/machine_comprehension/t5/model/t5-decoder-with-lm-head-12.onnx
+fi
+
 printf "\n[Compile Script]: Convert TF model to LLVM IR\n"
 onnx-mlir --EmitLLVMIR  --instrument-onnx-ops="ALL" --InstrumentBeforeAndAfterOp t5-encoder-12.onnx
 mlir-translate -mlir-to-llvmir t5-encoder-12.onnx.mlir > model.mlir.ll
 
 printf "\n[Compile Script]: Compile main driver program and link to TF model in LLVM IR\n"
-clang++ -DONNX_ML=1 input.c -o main.ll -O0 -S -emit-llvm -lonnx_proto -lprotobuf -I$ONNX_MLIR_SRC/include 
+clang++ -DONNX_ML=1 input.c -o main.ll -O0 -S -emit-llvm -lonnx_proto -lprotobuf -I$ONNX_MLIR_SRC/include
 llvm-link -o model.ll -S main.ll model.mlir.ll
 
 printf "\n[Compile Script]: Generate model.exe \n"
-/home/llvm-project/build/bin/llc -filetype=obj -o model.o model.ll -O0 --relocation-model=pic
-clang++ -o model.exe model.o -L/home/LLTFI/LLTFI/build/bin/../runtime_lib -lllfi-rt -lpthread -L /Debug/lib -Wl,-rpath /home/LLTFI/LLTFI/build/bin/../runtime_lib -I$ONNX_MLIR_SRC/include -O0 -lonnx_proto -lprotobuf -lcruntime -ljson-c
+$LLVM_DST_ROOT/bin/llc -filetype=obj -o model.o model.ll -O0 --relocation-model=pic
+clang++ -o model.exe model.o -L$LLFI_BUILD_ROOT/bin/../runtime_lib -lllfi-rt -lpthread -L /Debug/lib -Wl,-rpath $LLFI_BUILD_ROOT/bin/../runtime_lib -I$ONNX_MLIR_SRC/include -O0 -lonnx_proto -lprotobuf -lcruntime -ljson-c
 
 printf "\n[Compile Script]: Compilation complete\n"
---
+++ sample_programs/ml_sample_programs/nlp_models/t5-encoder/runllfiSingleInp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf llfi*
+inputSample=$1
+
+cp -r "inputs/input_$inputSample.pb" .
+
+$LLFI_BUILD_ROOT/bin/instrument --readable -L $ONNX_MLIR_BUILD/Debug/lib -lcruntime -ljson-c -lonnx_proto -lprotobuf model.ll
+$LLFI_BUILD_ROOT/bin/profile ./llfi/model-profiling.exe "input_$inputSample.pb" 0
+$LLFI_BUILD_ROOT/bin/injectfault ./llfi/model-faultinjection.exe "input_$inputSample.pb" 0

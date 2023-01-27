a4fd573361b3c143142c13eb58cf04a4a8b2cb20
Anushree Bannadabhavi <anushree.bannadabhavi@gmail.com>
2023-01-26 19:18:10 -0800

    Roberta-seq-classification model: Test and fix scripts
    Co-authored-by: uditagarwal97 <uditagarwal1997@gmail.com>

---
+++ sample_programs/ml_sample_programs/nlp_models/roberta-seq-classification-9/compile.sh
@@ -0,0 +1,23 @@
+printf "[Compile Script]: Getting the ONNX model\n"
+FILE=roberta-sequence-classification-9.onnx
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else
+    echo "$FILE does not exist."
+    wget https://github.com/onnx/models/raw/main/text/machine_comprehension/roberta/model/roberta-sequence-classification-9.onnx
+fi
+
+printf "\n[Compile Script]: Convert TF model to LLVM IR\n"
+onnx-mlir --EmitLLVMIR  --instrument-onnx-ops="ALL" --InstrumentBeforeAndAfterOp roberta-sequence-classification-9.onnx
+mlir-translate -mlir-to-llvmir roberta-sequence-classification-9.onnx.mlir > model.mlir.ll
+
+printf "\n[Compile Script]: Compile main driver program and link to TF model in LLVM IR\n"
+clang++ -DONNX_ML=1 input.c -o main.ll -O0 -S -emit-llvm -lonnx_proto -lprotobuf -I$ONNX_MLIR_SRC/include
+llvm-link -o model.ll -S main.ll model.mlir.ll
+
+printf "\n[Compile Script]: Generate model.exe \n"
+llc -filetype=obj -o model.o model.ll -O0 --relocation-model=pic
+clang++ -o model.exe model.o -L$LLFI_BUILD_ROOT/bin/../runtime_lib -lllfi-rt -lpthread -L /Debug/lib -Wl,-rpath $LLFI_BUILD_ROOT/bin/../runtime_lib -I$ONNX_MLIR_SRC/include -O0 -lonnx_proto -lprotobuf -lcruntime -ljson-c
+
+printf "\n[Compile Script]: Compilation complete\n"
+
---
+++ sample_programs/ml_sample_programs/nlp_models/roberta-seq-classification-9/createPredFile.py
@@ -0,0 +1,42 @@
+import numpy as np
+import glob
+import os
+import json
+
+ROOT = os.getcwd()
+LLFI_OUT = os.path.join(ROOT, 'llfi')
+PROG_OUT = os.path.join(LLFI_OUT, 'prog_output')
+
+def main():
+	# Get LLTFI outputs in listResArr
+	listResArr = []
+	list_of_files = sorted( filter( lambda x: os.path.isfile(os.path.join(PROG_OUT, x)),
+							os.listdir(PROG_OUT) ) )
+
+	for i in range(len(list_of_files)):
+		list_of_files[i] = os.path.join(PROG_OUT, list_of_files[i])
+
+	for filename in list_of_files:
+		with open(filename, "r") as read_file:
+			resultJson = json.load(read_file)
+
+			for key, value in resultJson.items():
+				listResArr.append(value['Data'])
+
+	listPreds = []
+	# Get Sentiment (positive/negative) prediction
+	for resIndex in range(len(listResArr)):
+		elem = listResArr[resIndex]
+		pred = np.argmax(elem)
+		if(pred == 0):
+			listPreds.append(f"Run #{resIndex} Prediction: Negative\n")
+		elif(pred == 1):
+			listPreds.append(f"Run #{resIndex} Prediction: Positive\n")
+
+	myfile = open('prediction/PredResult.txt', 'w')
+	myfile.writelines(listPreds)
+	myfile.close()
+
+
+if __name__ == "__main__":
+    main()

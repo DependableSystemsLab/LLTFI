1016853d56a19498d289ff8a6bda3d767a731f2d
Abraham Chan <abrahamchan2@gmail.com>
2024-06-15 13:30:08 -0700

    Add trained model to MNIST sample

--- sample_programs/ml_sample_programs/vision_models/mnist/compile.sh
+++ sample_programs/ml_sample_programs/vision_models/mnist/compile.sh
@@ -1,8 +1,12 @@
-printf "\n[Compile Script]: Generating TF model\n"
-python3 mnist-cnn.py
+if [ -f "$FILE" ]; then
+    echo "$FILE exists. Run ./clean.sh to retrain model."
+else
+    printf "\n[Compile Script]: Generating TF and ONNX models\n"
+    python3 mnist-cnn.py
+    python3 -m tf2onnx.convert --saved-model mnist-cnn.tf --output model.onnx
+fi
 
-printf "\n[Compile Script]: Convert TF model to LLVM IR\n"
-python3 -m tf2onnx.convert --saved-model mnist-cnn.tf --output model.onnx
+printf "\n[Compile Script]: Convert ONNX model to LLVM IR\n"
 python3 ../../../../tools/ExtendONNXModel.py --model_path ./model.onnx  --output_model_path ./extendedmodel.onnx > expected_op_seq.txt
 onnx-mlir --EmitLLVMIR extendedmodel.onnx --instrument-onnx-ops="ALL" --InstrumentBeforeAndAfterOp
 mlir-translate -mlir-to-llvmir extendedmodel.onnx.mlir > model.mlir.ll
